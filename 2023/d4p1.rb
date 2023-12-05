8bc6d27cdb61ee156dbf33311824e4a091c4a1a6
Jordan Miner <jminer7@gmail.com>
2023-12-05 01:24:58 -0600

    Add a solution to day 4 part 1

---
+++ 2023/d4p1.rb
@@ -0,0 +1,20 @@
+
+# Part 1 took me 10:48 PM - 10:59 PM, 1:15 AM - 1:23 AM (19 min)
+
+Card = Struct.new(:num, :winning_nums, :have_nums)
+
+def parse_card(line)
+    card_parts = line.split ":"
+    card_num = Integer(card_parts[0].delete_prefix("Card "))
+    winning_nums, have_nums = card_parts[1].split(" | ").map do |nums_list|
+        nums_list.split(" ").map { Integer(_1.strip) }
+    end
+    Card.new(card_num, winning_nums, have_nums)
+end
+
+input = IO.readlines("d4_input").map { |line| line.chomp }
+cards = input.map { parse_card _1 }
+card_points = cards.map { |card| 1 << (card.winning_nums.intersection(card.have_nums).length - 1) }
+p card_points
+sum = card_points.inject(0) { _1 + _2 }
+puts sum
